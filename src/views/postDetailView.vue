b28e63cd5a5d216986e40031e22ae687e2576ce2
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2023-05-12 00:29:01 +0600

    complete router

---
+++ src/views/postDetailView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a PostDetail Page</h1>
+    <p>Display the content of post with id of {{ $route.params.id }} here!</p>
+    <div>
+      <button @click="showPostId">Show post id</button>
+    </div>
+    <div>
+      <button @click="goHome3Seconds">Go Home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to First Post</button>
+    </div>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+</template>
+
+<script setup>
+/*
+  imports
+  */
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+const showPostId = () => {
+  alert(`The ID of this post is: ${route.params.id}`);
+};
+
+const goHome3Seconds = () => {
+  setTimeout(() => {
+    router.push({ name: "home" });
+  }, 3000);
+};
+
+const goToFirstPost = () => {
+  router.push({
+    name: "postDetail",
+    params: {
+      id: "id1",
+    },
+  });
+};
+</script>
---
+++ src/views/postsView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="posts">
+    <h1>Post page</h1>
+
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/id1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/id2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/id3">Post 3</RouterLink>
+      </li>
+    </ul>
+
+    <textarea v-autofocus />
+  </div>
+</template>
+
+<script setup>
+/*
+imports
+*/
+import { vAutofocus } from "@/directives/vAutofocus";
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
